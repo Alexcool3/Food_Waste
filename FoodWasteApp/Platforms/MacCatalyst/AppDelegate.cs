1719b7543301da4c552a7c5896769467af9ccf30
Alexcool3 <Alex33@live.dk>
2023-07-07 21:46:41 +0200

    Add project files.

---
+++ FoodWasteApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FoodWasteApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
